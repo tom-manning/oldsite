b7edfb6c0c8b3a8669cbae09dbf2e031313d2888
Tom Manning <tmanning20@gmail.com>
2016-12-13 08:36:35 +0000

    lots of changes

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
 $('#mail').on("click", function(){
     window.open("mailto:copytommanning@gmail.com?subject=I just saw your site")
 });
+
+// wet code alert
+$('#hmail').on("click", function(){
+    window.open("mailto:copytommanning@gmail.com?subject=I just saw your site")
+});
+
